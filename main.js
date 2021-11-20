61a87e320bb256abe6dcdc0a04b049550022547a
Calebramriez <80371493+Calebramriez@users.noreply.github.com>
2021-11-19 17:48:41 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+x=0;
+y=0;
+draw_circle="";
+draw_rect="";
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition;
+function start(){
+document.getElementById("status").innerHTML=" system is listening please speak";
+recognition.start();
+}
+recognition.onresult=function(event){
+console.log(event);
+var content=event.results[0][0].transcript;
+document.getElementById("status").innerHTML="speech has been recognised as:" +content;
+if(content=="Circle"){
+x=Math.floor(Math.random()*900);
+y=Math.floor(Math.random()*600);
+document.getElementById("status").innerHTML="started drawing circle";
+draw_circle="set";
+}
+if(content=="rectangle"){
+x=Math.floor(Math.random()*900);
+y=Math.floor(Math.random()*600);
+document.getElementById("status").innerHTML="started drawing rectangle";
+draw_rect="set";
+}
+}
+function setup(){
+canvas=createCanvas(900,600);
+
+}
+function draw(){
+if(draw_circle=="set"){
+radius=Math.floor(Math.random()*100);
+document.getElementById("status").innerHTML="circle is drawn";
+circle(x,y,radius);
+draw_circle="";
+}
+if(draw_rect=="set"){
+    
+    document.getElementById("status").innerHTML="rectangle is drawn";
+    rect(x,y,70,50);
+    draw_rect="";
+    }
+}
